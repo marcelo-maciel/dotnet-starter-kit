63a9f118b79ace861341c60a9bc718c00514c125
Marcelo M. Maciel <4993482+marcelo-maciel@users.noreply.github.com>
2026-07-20 03:30:14 -0300

    feat(identity): migration adding nullable Locale column to Users

---
+++ src/Host/FSH.Starter.Migrations.PostgreSQL/Identity/20260720062947_AddUserLocale.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSH.Starter.Migrations.PostgreSQL.Identity
+{
+    /// <inheritdoc />
+    public partial class AddUserLocale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Locale",
+                schema: "identity",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Locale",
+                schema: "identity",
+                table: "Users");
+        }
+    }
+}
